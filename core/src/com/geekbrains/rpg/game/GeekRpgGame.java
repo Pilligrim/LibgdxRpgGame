140d39323309c9771d954763ffbc7f9bbd0e5ef9
Шаповал Павел Николавеич <shapoval.p.n@sberbank.ru>
2020-03-04 13:55:26 +0300

    apple added

--- core/src/com/geekbrains/rpg/game/GeekRpgGame.java
+++ core/src/com/geekbrains/rpg/game/GeekRpgGame.java
@@ -3,12 +3,10 @@ package com.geekbrains.rpg.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Vector2;
 
 public class GeekRpgGame extends ApplicationAdapter {
     private SpriteBatch batch;
@@ -16,6 +14,7 @@ public class GeekRpgGame extends ApplicationAdapter {
     private TextureAtlas atlas;
     private TextureRegion textureGrass;
     private Hero hero;
+    private Apple apple;
 
     // Домашнее задание:
     // 0. Разобраться с кодом
@@ -30,6 +29,7 @@ public class GeekRpgGame extends ApplicationAdapter {
         this.hero = new Hero(atlas);
         this.textureGrass = atlas.findRegion("grass");
         this.font32 = new BitmapFont(Gdx.files.internal("font32.fnt"));
+        this.apple = new Apple(atlas);
     }
 
     @Override
